46dd5634319f7c881006d0b1c2e7f69233eaffce
Nirmal2000 <nirmalvelu2000@gmail.com>
2025-08-08 21:01:05 +0530

    download fix

--- api/endpoints.py
+++ api/endpoints.py
@@ -300,7 +300,7 @@ class OrchestratorEndpoints:
                                                 filepath = match.group(1)
                                                 try:
                                                     # Connect to sandbox and get download URL
-                                                    sandbox = sandbox_info.sandbox
+                                                    sandbox = self.sandbox_manager.active_sandboxes[sandbox_info.sandbox_id]
                                                     print(f"Downloading file {filepath} from sandbox {sandbox_info.sandbox_id}")
                                                     signed_url = sandbox.download_url(path=filepath)
                                                     # Replace the chunk with download URL
